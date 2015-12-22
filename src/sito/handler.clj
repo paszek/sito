8f72dc63f431c7cd26db64e2d30d82e1646798ff
Pawel Loba <pawel.loba@logview.pl>
2015-12-22 20:49:57 +0100

    ini2

--- src/sito/handler.clj
+++ src/sito/handler.clj
@@ -9,3 +9,11 @@
 
 (def app
   (wrap-defaults app-routes site-defaults))
+
+(defn -main [& [port]]
+  (let [port (Integer. (or port (env :port) 5000))]
+    (jetty/run-jetty (site #'app) {:port port :join? false})))
+
+;; For interactive development:
+;; (.stop server)
+;; (def server (-main))
